ee1f7230a913e21f25c6895731ef6aa8ca4a4c59
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-06-16 13:28:58 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -9,7 +9,7 @@ function App() {
   //Error Handling
   const [ error, setError ] = useState( null );
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback( async () => {
     setIsLoading(true);
     setError(null);
 
@@ -35,7 +35,11 @@ function App() {
       setError(error.message);
     }
       setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
